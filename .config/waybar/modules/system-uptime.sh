acec37033a441adfe554464e0cc04b1633038497
mroetsc <mail@mattes-roetschke.de>
2024-04-02 14:51:34 +0200

    Added custom waybar module for system uptime

---
+++ .config/waybar/modules/system-uptime.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+uptime_seconds=$(awk '{print $1}' /proc/uptime)
+
+uptime_hours_minutes=$(date -u -d @${uptime_seconds} +"%H:%M")
+
+uptime_detailed=$(date -u -d @${uptime_seconds} +"%j days, %H hours, %M minutes, %S seconds")
+
+echo "{\"text\": \"$uptime_hours_minutes\", \"tooltip\": \"Uptime: $uptime_detailed\"}"
