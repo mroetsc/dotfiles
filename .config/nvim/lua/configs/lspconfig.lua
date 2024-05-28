95bf302f69cf7fabbd653b8782f69a17a7779fbe
mroetsc <mail@mattes-roetschke.de>
2024-05-28 12:13:59 +0200

    Updated neovim config to better work with Rust

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,8 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "pyright", "jinja_lsp" }
+local util = require "lspconfig/util"
+local servers = { "html", "cssls", "pyright", "jinja_lsp", }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
