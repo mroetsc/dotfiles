c9738ffff66adbe2d3128623f2b92c089352b91b
mroetsc <mail@mattes-roetschke.de>
2024-10-11 10:04:50 +0200

    nvim: fixed leaving visual mode after indent change

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -4,3 +4,6 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+
+vim.keymap.set('v', '>', '>gv', { noremap = true })
+vim.keymap.set('v', '<', '<gv', { noremap = true })
