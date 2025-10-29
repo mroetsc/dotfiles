e31a4f9ffcab224bd12e0b307178b921c8a0dbeb
mroetsc <mail@mattes-roetschke.de>
2025-10-29 06:52:29 +0100

    nvim: treat mdx as markdown for treesitter

--- .config/nvim/lua/plugins/specs/treesitter.lua
+++ .config/nvim/lua/plugins/specs/treesitter.lua
@@ -4,6 +4,17 @@ return {
     ensure_installed = {
       "vim", "lua", "vimdoc",
       "html", "css",
+      "markdown", "markdown_inline",
     },
   },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+
+    -- Treat .mdx files as markdown
+    vim.filetype.add({
+      extension = {
+        mdx = "markdown",
+      },
+    })
+  end,
 }
